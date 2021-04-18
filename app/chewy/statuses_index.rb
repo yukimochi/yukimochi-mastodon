a10712134fd51029879371a35335b4f54d71c88e
Naoki Kosaka <naoki_kosaka@yukimochi.jp>
2021-04-18 09:39:27 +0900

    Optimize toot search, increase allow image size.

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -5,10 +5,6 @@ class StatusesIndex < Chewy::Index
 
   settings index: { refresh_interval: '15m' }, analysis: {
     filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
       english_stemmer: {
         type: 'stemmer',
         language: 'english',
@@ -18,17 +14,28 @@ class StatusesIndex < Chewy::Index
         language: 'possessive_english',
       },
     },
+    tokenizer: {
+      ja_tokenizer: {
+        type: 'kuromoji_tokenizer',
+        mode: 'search'
+      },
+    },
     analyzer: {
       content: {
-        tokenizer: 'uax_url_email',
+        tokenizer: 'ja_tokenizer',
+        type: 'custom',
+        char_filter: %w(
+          icu_normalizer
+        ),
         filter: %w(
+          kuromoji_stemmer
+          kuromoji_part_of_speech
           english_possessive_stemmer
-          lowercase
-          asciifolding
-          cjk_width
-          english_stop
           english_stemmer
         ),
+      },
+      ja_default_analyzer: {
+        tokenizer: 'kuromoji_tokenizer',
       },
     },
   }
@@ -59,7 +66,7 @@ class StatusesIndex < Chewy::Index
     field :id, type: 'long'
     field :account_id, type: 'long'
 
-    field :text, type: 'text', value: ->(status) { [status.spoiler_text, extract_status_plain_text(status)].concat(status.ordered_media_attachments.map(&:description)).concat(status.preloadable_poll ? status.preloadable_poll.options : []).join("\n\n") } do
+    field :text, type: 'text', analyzer: 'ja_default_analyzer', value: ->(status) { [status.spoiler_text, extract_status_plain_text(status)].concat(status.ordered_media_attachments.map(&:description)).concat(status.preloadable_poll ? status.preloadable_poll.options : []).join("\n\n") } do
       field :stemmed, type: 'text', analyzer: 'content'
     end
 
