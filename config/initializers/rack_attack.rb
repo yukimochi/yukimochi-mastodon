7ba3d3e5c00dfd3868320a1a5ac2817513005649
Naoki Kosaka <naoki_kosaka@yukimochi.jp>
2023-04-09 09:32:52 +0900

    feat: configurable api limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,7 +3,10 @@
 require 'doorkeeper/grape/authorization_decorator'
 
 class Rack::Attack
+  API_THROTTLE_LIMIT_RATIO = ENV.fetch('API_THROTTLE_LIMIT_RATIO', 1).to_i
+
   class Request
+
     def authenticated_token
       return @authenticated_token if defined?(@authenticated_token)
 
@@ -66,11 +69,11 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
-  throttle('throttle_authenticated_api', limit: 1_500, period: 5.minutes) do |req|
+  throttle('throttle_authenticated_api', limit: 1_500 * API_THROTTLE_LIMIT_RATIO, period: 5.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
 
-  throttle('throttle_per_token_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_per_token_api', limit: 300 * API_THROTTLE_LIMIT_RATIO, period: 5.minutes) do |req|
     req.authenticated_token_id if req.api_request?
   end
 
@@ -78,11 +81,11 @@ class Rack::Attack
     req.throttleable_remote_ip if req.api_request? && req.unauthenticated?
   end
 
-  throttle('throttle_api_media', limit: 30, period: 30.minutes) do |req|
+  throttle('throttle_api_media', limit: 30 * API_THROTTLE_LIMIT_RATIO, period: 30.minutes) do |req|
     req.authenticated_user_id if req.post? && req.path.match?(%r{\A/api/v\d+/media\z}i)
   end
 
-  throttle('throttle_media_proxy', limit: 30, period: 10.minutes) do |req|
+  throttle('throttle_media_proxy', limit: 30 * API_THROTTLE_LIMIT_RATIO, period: 10.minutes) do |req|
     req.throttleable_remote_ip if req.path.start_with?('/media_proxy')
   end
 
@@ -90,7 +93,7 @@ class Rack::Attack
     req.throttleable_remote_ip if req.post? && req.path == '/api/v1/accounts'
   end
 
-  throttle('throttle_authenticated_paging', limit: 300, period: 15.minutes) do |req|
+  throttle('throttle_authenticated_paging', limit: 300 * API_THROTTLE_LIMIT_RATIO, period: 15.minutes) do |req|
     req.authenticated_user_id if req.paging_request?
   end
 
@@ -101,7 +104,7 @@ class Rack::Attack
   API_DELETE_REBLOG_REGEX = %r{\A/api/v1/statuses/\d+/unreblog\z}
   API_DELETE_STATUS_REGEX = %r{\A/api/v1/statuses/\d+\z}
 
-  throttle('throttle_api_delete', limit: 30, period: 30.minutes) do |req|
+  throttle('throttle_api_delete', limit: 30 * API_THROTTLE_LIMIT_RATIO, period: 30.minutes) do |req|
     req.authenticated_user_id if (req.post? && req.path.match?(API_DELETE_REBLOG_REGEX)) || (req.delete? && req.path.match?(API_DELETE_STATUS_REGEX))
   end
 
